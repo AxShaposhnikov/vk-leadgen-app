58010d90d78ed993d559eb77be6e65fd4e80e7cb
AxShaposhnikov <alexxforcode@gmail.com>
2025-12-13 02:17:20 +0300

    исправил vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/vk-leadgen-app/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
